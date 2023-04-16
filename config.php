d6ce94a23b70c66b78b82e93e4020c03d3b22d4e
AppiahEnoch <prignutt@gmail.com>
2023-04-16 16:23:17 -0700

    config

--- config.php
+++ config.php
@@ -1,15 +1,52 @@
 <?php
+
 $conn ="";
+
+        $DBhostname = "localhost";
+        $DBusername = "root";
+        $DBpassword = "";
+        $database = "car";
+        $DBport = "63943";  
+
+$remote_addr = $_SERVER['REMOTE_ADDR'];
+$remote_host = gethostbyaddr($remote_addr);
+
+if($remote_host=="AECleanCodes"){
+
+
+
     try {
 
-        $hostname = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "car";
-        $port = "63943";  
-       $conn = mysqli_connect($hostname, $username, $password, $database,$port) or die("Database connection failed");
+
+       $conn = mysqli_connect($DBhostname, $DBusername, $DBpassword, $database,$DBport) or die("Database connection failed");
  
     } catch (Throwable $th) {
         //throw $th;
     }
+
+
+exit;
+}
+
+
+// remote server
+
+    try {
+
+
+
+        $DBhostname = "localhost";
+        $DBusername = "root";
+        $DBpassword = "";
+        $database = "car";
+    
+
+       $conn = mysqli_connect($DBhostname, $DBusername, $DBpassword, $database) or die("Database connection failed");
+ 
+    } catch (Throwable $th) {
+        //throw $th;
+    }
+
+
+
 
