585f52766e6c0da2028a5b90947f8916abb9e29e
AppiahEnoch <prignutt@gmail.com>
2023-05-13 07:48:39 -0700

    rrr

--- config.php
+++ config.php
@@ -9,10 +9,7 @@ $remote_host = gethostbyaddr($remote_addr);
 
 
 
-
-
 if($remote_host=="AECleanCodes1"){
-
   
     try {
        $conn = mysqli_connect($DBhostname, $DBusername, $DBpassword, $database,$DBport) or die("Database connection failed");
@@ -24,16 +21,12 @@ if($remote_host=="AECleanCodes1"){
 }
 else{
 
-
     try {
 
-        
-        $DBhostname = "crysta.online";
-        $DBusername = "crystaon_1";
-        $DBpassword = "6t@BEd8ar~O$";
-        $database = "crystaon_1";
-      
-
+        $DBhostname = "";
+        $DBusername = "";
+        $DBpassword = "";
+        $database = "";
        $conn = mysqli_connect($DBhostname, $DBusername, $DBpassword, $database) or die("Database connection failed");
  
     } catch (Throwable $th) {
