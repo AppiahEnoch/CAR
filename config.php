f69261d2f9dbbf1155edc07fd387f3f7aa6295f2
AppiahEnoch <prignutt@gmail.com>
2023-05-14 04:27:29 -0700

    ttt

--- config.php
+++ config.php
@@ -3,20 +3,13 @@
 
 
 
-
-
 require_once('../ENV/env.php');
 
 $conn ="";
 
-
 $remote_addr = $_SERVER['REMOTE_ADDR'];
 $remote_host = gethostbyaddr($remote_addr);
 
-
-
-if($remote_host=="AECleanCodes1"){
-  
     try {
        $conn = mysqli_connect($DBhostname, $DBusername, $DBpassword, $database,$DBport) or die("Database connection failed");
  
@@ -24,21 +17,7 @@ if($remote_host=="AECleanCodes1"){
         //throw $th;
     }
 
-}
-else{
 
-    try {
-
-        $DBhostname = "crysta.online";
-        $DBusername = "crystao1_db1";
-        $DBpassword = "p3s4xgUH5!3V";
-        $database = "crystao1_db1";
-       $conn = mysqli_connect($DBhostname, $DBusername, $DBpassword, $database) or die("Database connection failed");
- 
-    } catch (Throwable $th) {
-        //throw $th;
-    }
-}
 
 
 
