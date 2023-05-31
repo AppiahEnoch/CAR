c3e59e5ada690efa8c02b40d722251aa1a61c062
AppiahEnoch <prignutt@gmail.com>
2023-05-31 04:49:30 -0700

    init

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
-
-
-
-
 require_once('../ENV/env.php');
-
 $conn ="";
-
 $remote_addr = $_SERVER['REMOTE_ADDR'];
 $remote_host = gethostbyaddr($remote_addr);
+if($remote_host == "AECleanCodes1"){
+$DBhostname = "localhost";
+$DBusername = "root";
+$DBpassword = "";
+$database = "car";
+$DBport = "63943"; 
+    
+}
 
     try {
        $conn = mysqli_connect($DBhostname, $DBusername, $DBpassword, $database,$DBport) or die("Database connection failed");
